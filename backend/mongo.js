c62c785dc49665379be2d87d4a62f3ad712c459b
Anette <anet1@rocketmail.com>
2021-01-20 12:13:25 +0000

    removed useless files

--- backend/mongo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>');
-  process.exit(1);
-}
-const password = process.argv[2];
-
-const url = ``;
